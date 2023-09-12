d400687a112cb89ecaa2167f7f007f32f43ee5eb
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-09-11 21:40:57 -0400

    Create nestjs app

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const PORT = process.env.PORT || 3000; 
+  const app = await NestFactory.create(AppModule, { // Creación de la app
+    cors: true,  // Cors
+  });
+
+  app.enableVersioning({  // Versionamiento
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder() // Documentación
+    .addBearerAuth()
+    .setTitle('AnimeList API')
+    .setDescription('Esta es la api de AnimeList')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config); // Documentación
+  SwaggerModule.setup('documentation', app, document); // Documentación
+
+  app.useGlobalPipes(new ValidationPipe()); // Validación de datos
+  await app.listen(PORT);
+}
+bootstrap();
