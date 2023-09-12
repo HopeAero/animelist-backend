e3238133877c6b99128f492c51db0ecb04204559
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-09-11 22:11:01 -0400

    Add DTO Users And Tag users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @ApiProperty()
+    @IsNotEmpty()
+    name: string;
+
+    @ApiProperty()
+    @IsEmail()
+    email: string;
+
+    @ApiProperty()
+    @MinLength(6)
+    password: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    role: string;
+}
